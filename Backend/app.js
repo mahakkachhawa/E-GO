76524b8fc8091896bd5711609c0371f72c9881a5
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-23 20:49:36 +0530

    add

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 connectToDb();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
