75c61dfbf833d62d5e2435a1a99d9722029dceae
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-23 19:54:21 +0530

    update

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -5,8 +5,8 @@ const blackListTokenModel = require('../models/blackListToken.model');
 const captainModel = require('../models/captain.model');
 
 
-module.exports.authUser = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+module.exports.authUser = async(req, res, next) => {
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
 
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
@@ -33,8 +33,8 @@ module.exports.authUser = async (req, res, next) => {
     }
 }
 
-module.exports.authCaptain = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+module.exports.authCaptain = async(req, res, next) => {
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
 
 
     if (!token) {
