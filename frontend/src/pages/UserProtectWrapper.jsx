e8638ea54ba0a1b0108c9a9cff0fbf50a3e5e643
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-24 22:01:17 +0530

    add

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,49 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { UserDataContext } from '../context/UserContext'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const UserProtectWrapper = ({
-    children
-}) => {
-    const token = localStorage.getItem('token')
-    const navigate = useNavigate()
-    const { user, setUser } = useContext(UserDataContext)
-    const [ isLoading, setIsLoading ] = useState(true)
+const UserProtectWrapper = ({ children }) => {
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!token) {
-            navigate('/login')
-        }
-
-        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then(response => {
-            if (response.status === 200) {
-                setUser(response.data)
-                setIsLoading(false)
-            }
-        })
-            .catch(err => {
-                console.log(err)
-                localStorage.removeItem('token')
-                navigate('/login')
-            })
-    }, [ token ])
-
-    if (isLoading) {
-        return (
-            <div>Loading...</div>
-        )
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/home');
     }
+  }, [navigate]);
 
-    return (
-        <>
-            {children}
-        </>
-    )
-}
+  return <>{children}</>;
+};
 
-export default UserProtectWrapper
+export default UserProtectWrapper;
